659221fb3b0932977b5affca421d379af4fc312e
Lajtha Balázs <lajthabalazs@yahoo.com>
2026-02-06 16:46:54 -0400

    Adjusted site selection layout

--- src/main/java/ca/lajthabalazs/pressure_integrity_test/ui/view/SiteSelectionStepPanel.java
+++ src/main/java/ca/lajthabalazs/pressure_integrity_test/ui/view/SiteSelectionStepPanel.java
@@ -2,59 +2,57 @@ package ca.lajthabalazs.pressure_integrity_test.ui.view;
 
 import ca.lajthabalazs.pressure_integrity_test.ui.viewmodel.NewTestWizardViewModel;
 import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.Component;
 import java.io.File;
-import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 class SiteSelectionStepPanel extends JPanel {
 
   private final NewTestWizardViewModel viewModel;
-  private final JTextField filePathField;
-  private final JButton browseButton;
+  private final JLabel filePathLabel;
+  private final JButton chooseButton;
 
   SiteSelectionStepPanel(NewTestWizardViewModel viewModel) {
     this.viewModel = viewModel;
     setBackground(Color.WHITE);
-    setLayout(new GridBagLayout());
-    GridBagConstraints gbc = new GridBagConstraints();
-    gbc.insets = new Insets(5, 5, 5, 5);
-    gbc.anchor = GridBagConstraints.WEST;
-    gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.weightx = 1;
+    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-    filePathField = new JTextField(40);
-    filePathField.setEditable(false);
-    filePathField.setBackground(Color.WHITE);
-    filePathField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(0xcccccc)));
-    gbc.gridx = 0;
-    gbc.gridy = 0;
-    add(filePathField, gbc);
+    chooseButton = new JButton("Choose site config");
+    chooseButton.addActionListener(e -> chooseSiteConfig());
+    chooseButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+    add(chooseButton);
 
-    browseButton = new JButton("Browse...");
-    browseButton.addActionListener(e -> browseForFile());
-    gbc.gridx = 1;
-    gbc.weightx = 0;
-    add(browseButton, gbc);
+    add(Box.createVerticalStrut(10));
+
+    filePathLabel = new JLabel();
+    filePathLabel.setForeground(new Color(0x666666));
+    filePathLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+    add(filePathLabel);
 
     updateFromViewModel();
   }
 
   void setEditable(boolean editable) {
-    browseButton.setEnabled(editable);
+    chooseButton.setEnabled(editable);
   }
 
   void updateFromViewModel() {
     File file = viewModel.getSelectedFile();
-    filePathField.setText(file != null ? file.getAbsolutePath() : "");
+    if (file != null) {
+      filePathLabel.setText(file.getAbsolutePath());
+      filePathLabel.setVisible(true);
+    } else {
+      filePathLabel.setText("");
+      filePathLabel.setVisible(false);
+    }
   }
 
-  private void browseForFile() {
+  private void chooseSiteConfig() {
     JFileChooser chooser = new JFileChooser();
     if (viewModel.getSelectedFile() != null) {
       chooser.setCurrentDirectory(viewModel.getSelectedFile().getParentFile());
